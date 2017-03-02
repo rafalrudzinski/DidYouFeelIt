1c2c56e157fc8f436ba2b457fbe40ebc9e4b8adf
Rafal Rudzinski <rrudzin1@asu.edu>
2017-03-01 17:46:03 -0800

    finished mainactivity and added strings and colors

--- app/src/main/java/com/example/rrudz/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/rrudz/didyoufeelit/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.rrudz.didyoufeelit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String USGS_REQUEST_URL =
+            "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // perform the HTTP request for earthquake data and process the response
+        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
+
+        // update the information displayed to the user
+        updateUi(earthquake);
+    }
+
+    /**
+     *  Update the UI with the given earthquake information
+     */
+    private void updateUi(Event earthquake) {
+        TextView titleTextView = (TextView) findViewById(R.id.title);
+        titleTextView.setText(earthquake.title);
+
+        TextView tsunamiTextView = (TextView) findViewById(R.id.number_of_people);
+        tsunamiTextView.setText(getString(R.string.num_of_people_felt_it, earthquake.numOfPeople));
+
+        TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
+        magnitudeTextView.setText(earthquake.perceivedStrength);
     }
 }
